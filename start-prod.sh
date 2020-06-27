fe15690edf373ab32010969a5a18ef7537816933
Brent Groves <brent.groves@gmail.com>
2020-06-27 13:37:24 -0400

    Added/Changed Code

--- start-prod.sh
+++ start-prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-( cd /home/node/App13319/Feat13319 && npm start) & ( cd /home/node/App13319/Plex13319 && node app) & ( cd /home/node/App13319/InsKep13319 && node app) & ( cd /home/node/App13319/Alarms13319 && node app) & ( cd /home/node/App13319/Kep13319 && node app) 
+( cd /home/node/App13319/Feat13319 && npm start) & ( cd /home/node/App13319/Plex13319 && node app) & ( cd /home/node/App13319/InsKep13319 && node app) & ( cd /home/node/App13319/Alarms13319 && node app) & ( cd /home/node/App13319/Kep13319 && sleep 30 && node app) 
 #  > /dev/null 2>&1 
