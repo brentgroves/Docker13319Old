d2553b6cc3b50bc001c9d96adb142303a70bae86
Brent Groves <brent.groves@gmail.com>
2020-09-12 09:27:17 -0400

    bug fix

--- start-prod.sh
+++ start-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-( cd /home/node/App13319/Feat13319 && npm start) & ( cd /home/node/App13319/InsKep13319 && node app) & ( cd /home/node/App13319/SetupContainer13319 && node app) & ( cd /home/node/App13319/Plex13319 && node app) & ( cd /home/node/App13319/CompareContainer13319 && node app) & ( cd /home/node/App13319/Alarms13319 && sleep 1m && node app) & ( cd /home/node/App13319/Kep13319 && sleep 1m && node app) & ( cd /home/node/App13319/UDP13319 && sleep 1m && node app) 
+( cd /home/node/App13319/Feat13319 && npm start) & ( cd /home/node/App13319/InsKep13319 && node app) & ( cd /home/node/App13319/Tracker13319 && node app) & ( cd /home/node/App13319/SetupContainer13319 && node app) & ( cd /home/node/App13319/Plex13319 && node app) & ( cd /home/node/App13319/CompareContainer13319 && node app) & ( cd /home/node/App13319/Alarms13319 && sleep 1m && node app) & ( cd /home/node/App13319/Kep13319 && sleep 1m && node app) & ( cd /home/node/App13319/UDP13319 && sleep 1m && node app) 
 
 #  > /dev/null 2>&1 
