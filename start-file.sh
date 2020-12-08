045fb85ea67b521a567266874f83ba5fd67939f3
Brent Groves <brent.groves@gmail.com>
2020-12-08 17:03:23 -0500

    Initial Creation

---
+++ start-file.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+( cd /home/node/App13319/Feat13319 && npm start)  & ( cd /home/node/App13319/Tracker13319 && node app) & ( cd /home/node/App13319/UDP13319 && sleep 1m && node app) >> /home/node/App13319/app.log
+# ( cd /home/node/App13319/UDP13319 && sleep 1m && node app) 
+# ( cd /home/node/App13319/Feat13319 && npm start)  & ( cd /home/node/App13319/React13319 && sleep 10s && npm start)
+# ( cd /home/node/App13319/Feat13319 && npm start) & ( cd /home/node/App13319/Tracker13319 && node app) & ( cd /home/node/App13319/UDP13319 && sleep 1m && node app) & ( cd /home/node/App13319/React13319 && sleep 1m && npm start)
+# ( cd /home/node/App13319/Feat13319 && npm start) & ( cd /home/node/App13319/UDP13319 && sleep 1m && node app) & ( cd /home/node/App13319/Tracker13319 && node app) & ( cd /home/node/App13319/React13319 && sleep 1m && npm start)
+# ( cd /home/node/App13319/Feat13319 && npm start) & ( cd /home/node/App13319/InsKep13319 && node app) & ( cd /home/node/App13319/SetupContainer13319 && node app) & ( cd /home/node/App13319/Plex13319 && node app) & ( cd /home/node/App13319/CompareContainer13319 && node app) & ( cd /home/node/App13319/React13319 && sleep 1m && npm start) & ( cd /home/node/App13319/Alarms13319 && sleep 1m && node app) & ( cd /home/node/App13319/Kep13319 && sleep 1m && node app) & ( cd /home/node/App13319/UDP13319 && sleep 1m && node app) 
+# Add this to the end to suppress all output
+#  > /dev/null 2>&1 
