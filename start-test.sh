57988e4670a3e70b789dc1d154d8b20d191c3ba4
Brent Groves <brent.groves@gmail.com>
2021-01-28 16:26:13 -0500

    Added/Changed source code

--- start-test.sh
+++ start-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ( cd /home/node/App13319/Feat13319 && npm start)  & ( cd /home/node/App13319/Tracker13319 && node app) & ( cd /home/node/App13319/UDP13319 && sleep 1m && node app)
 # ( cd /home/node/App13319/UDP13319 && sleep 1m && node app) 
-( cd /home/node/App13319/Feat13319 && npm start)  & ( cd /home/node/App13319/React13319 && sleep 30s && npm start)
+( cd /home/node/App13319/Feat13319 && npm start)  
 
 # ( cd /home/node/App13319/Feat13319 && npm start) & ( cd /home/node/App13319/Tracker13319 && node app) & ( cd /home/node/App13319/UDP13319 && sleep 1m && node app) & ( cd /home/node/App13319/React13319 && sleep 1m && npm start)
 # ( cd /home/node/App13319/Feat13319 && npm start) & ( cd /home/node/App13319/UDP13319 && sleep 1m && node app) & ( cd /home/node/App13319/Tracker13319 && node app) & ( cd /home/node/App13319/React13319 && sleep 1m && npm start)
